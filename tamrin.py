f588827e29e8e75ac7a226740bdbeed65e084b30
nargesmohamadi <120326755+nargesmohamadi@users.noreply.github.com>
2022-12-13 03:22:31 -0800

    Add files via upload

---
+++ tamrin.py
@@ -0,0 +1,15 @@
+x="hi how are you my friend?"
+print(x)
+Number_h=len(x.split(" "))
+Number_k=len(x)
+a=x.replace('r','p')
+print("Number_h",Number_h)
+print("Number_k",Number_k)
+print("replace",a)
+
+print()
+
+q="hi what up? nothing . where are you going? university."
+number_j=len(q.split(","))
+print(q)
+print("number_j",number_j)
